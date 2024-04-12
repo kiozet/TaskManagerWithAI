b74d3c6b1326b8711d5d204f2363b8bdfc4746f9
kiozet <volosin2005@gmail.com>
2024-04-12 20:52:38 +0300

    registration waiting for frontend be done

--- registration.py
+++ registration.py
@@ -1,29 +1,69 @@
-'''The authorization part of a program'''
+"""The authorization part of a program"""
+
 import sqlite3
 
 
-def createTable(cursor: sqlite3.Cursor):
-    cursor.execute('''
+def createTable(connection: sqlite3.Connection, cursor: sqlite3.Cursor):
+    """Create table func, args: connection: sqlite3.Connection, cursor: sqlite3.Cursor from your
+    sqlite3 db"""
+
+    cursor.execute(
+        """
     CREATE TABLE IF NOT EXISTS Users (
     email TEXT NOT NULL,
     password TEXT NOT NULL
     )
-    ''')
+    """
+    )
+
+    connection.commit()
 
 
-def registrationUser(cursor: sqlite3.Cursor):
-    
+def emailValidation(email: str) -> bool:
+    if email.count("@") == 1 and email.count(".") == 1:  # write with fnmatch
+        return True
+
+    else:
+        return False
+
+
+def registrationUser(connection: sqlite3.Connection, cursor: sqlite3.Cursor) -> bool:
+    """User registration function by validate it and insert it into table.
+    args: connection: sqlite3.Connection, cursor: sqlite3.Cursor from your sqlite3 db
+    """
+
+    # input email and password, waiting to frontend be done
+    email = str(input())
+    password = str(input())
+
+    if emailValidation(email) == True and password != "":
+        cursor.execute(
+            "INSERT INTO Users (email, password) VALUES (?, ?)",
+            (
+                email,
+                password,
+            ),
+        )
+        connection.commit()
+
+        return True
+
+    else:
+        print("bad email or password = ''")
+        return False
+
+
 def main():
-    '''
-    This is main function of program:
-    connect with db
-    '''  
-    
+    """
+    This is the main function of program
+    """
+
     db = "authorization_test.db"
     connection = sqlite3.connect(db)
     cursor = connection.cursor()
-    
-    createTable(cursor)    
+
+    createTable(connection, cursor)
+    registrationUser(connection, cursor)
 
 
 if __name__ == "__main__":
