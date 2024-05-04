758213b370b472fae9ee518604295b45b617732b
kiozet <volosin2005@gmail.com>
2024-05-04 17:19:06 +0300

    mainwindow test.py

--- ui/test.py
+++ ui/test.py
@@ -2,10 +2,12 @@ from PyQt6.QtWidgets import *
 from PyQt6 import uic
 import sys
 
+
 class App(QFrame):
     def __init__(self):
         super().__init__()
-        uic.loadUi('auth.ui', self)
+        uic.loadUi('MainWindowTaskManager1.0.ui', self)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
