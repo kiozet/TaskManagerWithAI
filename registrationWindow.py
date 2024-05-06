31fcaa8f19c84f2fe87454d509b7c48f1d00d04a
kiozet <volosin2005@gmail.com>
2024-05-06 20:48:03 +0300

    pep8 update

--- registrationWindow.py
+++ registrationWindow.py
@@ -9,67 +9,75 @@ import time
 class LineEditData(QRunnable):
     def __init__(self) -> None:
         super(LineEditData, self).__init__()
-        
+
         self.threadpool = QThreadPool()
-        
-    
+
     @pyqtSlot()
     def run(self):
-        privUsrName = ''
-        privEmail = ''
-        privPassword =''
+        privUsrName = ""
+        privEmail = ""
+        privPassword = ""
         gifState = False
-        
+
         while True:
-            usrNameBool = (displayFrame.usr_name_text_reg.text() != "") and (displayFrame.usr_name_text_reg.text() != privUsrName)
-            emailBool = (displayFrame.email_text_reg.text() != "") and (displayFrame.email_text_reg.text() != privEmail)
-            passwordBool = (displayFrame.password_text_reg.text() != "") and (displayFrame.password_text_reg.text() != privPassword)
-            
-            if  ((usrNameBool == True)or (emailBool == True) or (passwordBool == True)) and (gifState == False):
+            usrNameBool = (displayFrame.usr_name_text_reg.text() != "") and (
+                displayFrame.usr_name_text_reg.text() != privUsrName
+            )
+            emailBool = (displayFrame.email_text_reg.text() != "") and (
+                displayFrame.email_text_reg.text() != privEmail
+            )
+            passwordBool = (displayFrame.password_text_reg.text() != "") and (
+                displayFrame.password_text_reg.text() != privPassword
+            )
+
+            if (
+                (usrNameBool == True) or (emailBool == True) or (passwordBool == True)
+            ) and (gifState == False):
                 displayFrame.gifStateSignalEmit(True)
                 gifState = True
+                
                 privUsrName = displayFrame.usr_name_text_reg.text()
                 privEmail = displayFrame.email_text_reg.text()
                 privPassword = displayFrame.password_text_reg.text()
-                time.sleep(0.6)
                 
+                time.sleep(0.6)
+
             else:
                 gifState = False
-                displayFrame.gifStateUpdater(False)        
-        
-    
+                displayFrame.gifStateUpdater(False)
+
+
 class RegistrationWindow(QFrame):
     gifStateSignal = pyqtSignal(bool)
-    
+
     def __init__(self) -> None:
         super().__init__()
-        uic.loadUi('ui/registr.ui', self)
-        
+        uic.loadUi("ui/registr.ui", self)
+
         self.movie = QMovie("content/pedro-racoon.gif")
         self.GIF.setMovie(self.movie)
-        
+
         self.threadpool = QThreadPool()
         self.gifAnimation()
-        
-        self.gifStateSignal.connect(self.gifStateUpdater)  
-        
+
+        self.gifStateSignal.connect(self.gifStateUpdater)
+
     def gifStateSignalEmit(self, state: bool) -> None:
         self.gifStateSignal.emit(state)
-        
+
     def gifAnimation(self):
         gifAnimation = LineEditData()
         self.threadpool.start(gifAnimation)
-                
-                
+
     def gifStateUpdater(self, state: bool) -> None:
         if state == True:
             self.movie.start()
-            
+
         else:
             self.movie.setPaused(True)
-              
-        
-if __name__ == '__main__':  
+
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     displayFrame = RegistrationWindow()
     displayFrame.show()
