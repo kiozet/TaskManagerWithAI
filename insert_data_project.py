d09cddae0149074a698519b48f5e270a6163246a
Kirya <efremenko.kirill2005@gmail.com>
2024-05-23 20:41:01 +0300

    Update insert_data_project.py
    Реализовал дату обновления таска

--- insert_data_project.py
+++ insert_data_project.py
@@ -1,13 +1,14 @@
-import sqlite3, api_with_button
+import sqlite3, api_with_button, datetime
 
 
 def insertDataProject(cursor: sqlite3.Cursor):
     # По нажатию кнопки, в случае Паши - Enter - я буду считывать информацию с тайтла таска, а затем буду считывать информацию с вкладки ниже
     if buttonEnter.clicked is True:
+        currentDate = datetime.datetime.now().strftime('%Y-%m-%d')
         connection = sqlite3.connect("project.db")
         taskTitle = lineEdit.text()
         taskDescription = api_with_button.GeneratingPoints(taskTitle)
         cursor.execute(
-            """INSERT INTO task (title, description) VALUES ({taskTitle}, {taskDescription})
+            """INSERT INTO task (title, description, date) VALUES ({taskTitle}, {taskDescription}, {currentDate})
 """
         )
