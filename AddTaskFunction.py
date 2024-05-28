350466c1ea23ac2b9b8b46a1f3cac40d9572631f
pavel <149060946+pavelchervonenko@users.noreply.github.com>
2024-05-28 03:55:07 +0300

    Функции добавления и удаления задач в MainWindow
    Функции добавления и удаления задач в MainWindow

--- AddTaskFunction.py
+++ AddTaskFunction.py
@@ -1,7 +1,7 @@
 from PyQt6 import QtWidgets
 from PyQt6.QtCore import Qt, pyqtSignal
 from PyQt6.QtWidgets import QVBoxLayout, QWidget, QGroupBox
-import MainWindow
+import MainWindowTaskManager
 import TaskWidget
 import sys
 
@@ -9,6 +9,7 @@ import sys
 class TaskWidget(QtWidgets.QWidget, TaskWidget.Ui_TaskWidget):
 
     delete = pyqtSignal(int)
+    choose = pyqtSignal(int)
 
     def __init__(self, id_task: int):
         super().__init__()
@@ -16,12 +17,16 @@ class TaskWidget(QtWidgets.QWidget, TaskWidget.Ui_TaskWidget):
         self.id_task = id_task
         self.groupBox.setTitle(str(id_task))
         self.pushButtonDelete.clicked.connect(self.DeleteWidget)
+        self.pushButtonChoose.clicked.connect(self.ChooseWidget)
 
     def DeleteWidget(self):
         self.delete.emit(self.id_task)
 
+    def ChooseWidget(self):
+        self.choose.emit(self.id_task)
 
-class MainWindow(QtWidgets.QMainWindow, MainWindow.Ui_MainWindow):
+
+class MainWindow(QtWidgets.QFrame, MainWindowTaskManager.Ui_Frame):
 
     def __init__(self):
         super().__init__()
@@ -33,17 +38,24 @@ class MainWindow(QtWidgets.QMainWindow, MainWindow.Ui_MainWindow):
     def AddTaskWidget(self):
         self.id_task += 1
         widget = TaskWidget(self.id_task)
-        self.TaskWidgetLayout.addWidget(widget)
+        self.verticalLayoutToDo.addWidget(widget)
         widget.delete.connect(self.DeleteWidget)
+        widget.choose.connect(self.ChooseWidget)
         self.task_widgets[self.id_task] = widget
 
     def DeleteWidget(self, id_task: int):
         widget = self.task_widgets.get(id_task)
         if widget:
-            self.TaskWidgetLayout.removeWidget(widget)
+            self.verticalLayoutToDo.removeWidget(widget)
             widget.deleteLater()
 
             del self.task_widgets[id_task]
+
+    def ChooseWidget(self, id_task: int):
+        widget = self.task_widgets.get(id_task)
+        if widget:
+            self.verticalLayoutToDo.removeWidget(widget)
+            self.verticalLayoutToDo.insertWidget(0, widget)
 
 
 def main():
